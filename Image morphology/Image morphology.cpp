727c7ab72323a0f40d03496df6a9808b67fbbe31
晨曦 <1179348863@qq.com>
2020-03-20 15:13:48 +0800

    练习3

--- Image morphology/Image morphology.cpp
+++ Image morphology/Image morphology.cpp
@@ -17,8 +17,41 @@ int main()
 	cv::Mat centrMat;
 	cv::Mat resultMat;
 	//二值化
-	bitwise_not(srcMat, srcMat);//注意前景为白色
-	cv::threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+
+
+	// 对图像进行所有像素用 （255- 像素值）
+	Mat invertImage;
+	srcMat.copyTo(invertImage);
+
+	// 获取图像宽、高
+	int channels = srcMat.channels();
+	int rows = srcMat.rows; //高---行
+	int col = srcMat.cols;//宽---列
+	cout << channels << " " << rows << endl;
+	int cols = srcMat.cols * channels;
+	cout << cols << endl;
+	if (srcMat.isContinuous()) {
+		cols *= rows;
+		rows = 1;
+	}
+
+	// 每个像素点的每个通道255取反（0-255（黑-白））
+
+	uchar* p1;
+	uchar* p2;
+	for (int row = 0; row < rows; row++) {
+		p1 = srcMat.ptr<uchar>(row);// 获取像素指针
+		p2 = invertImage.ptr<uchar>(row);
+		for (int col = 0; col < cols; col++) {
+			*p2 = 255 - *p1; // 取反
+			p2++;
+			p1++;
+		}
+	}
+
+
+//	bitwise_not(srcMat, srcMat);//注意前景为白色
+	cv::threshold(invertImage, binaryMat, 0, 255, THRESH_OTSU);
 	Mat kernel= getStructuringElement(MORPH_RECT, Size(11, 11));
 	morphologyEx(binaryMat, dstImage_Open, 2, kernel,Point(-1,-1),1);//开运算
 
