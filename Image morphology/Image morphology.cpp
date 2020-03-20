7a1ecffdd3a9c0b7f3b893349bc302751d44abe3
晨曦 <1179348863@qq.com>
2020-03-20 09:37:51 +0800

    课前准备

--- Image morphology/Image morphology.cpp
+++ Image morphology/Image morphology.cpp
@@ -318,7 +318,7 @@ void morphology(int argc, char **argv)
 	cvDestroyWindow("Open/Close");
 	cvDestroyWindow("Erode/Dilate");
 	return ;}
-
+void cvErode(IplImage* src, IplImage* dst, IplConvKernel* B = NULL, int iterations = 1);
 int main()
 {   
 	Mat  g_grayImage, g_dstImage;
@@ -343,6 +343,120 @@ int main()
     std::cout << "Hello World!\n";
 	imshow("Hello World", g_grayImage);
 	waitKey(0);//等待用户按键
+
+
+	cv::Mat binaryMat;
+	cv::Mat labelMat;
+	cv::Mat statsMat;
+	cv::Mat centrMat;
+	cv::Mat resultMat;
+	cv::Mat srcMat = imread("E:\\5.png", 0);
+	element = cvCreateStructuringElementEx(1 * 2 + 1, 1 * 2 + 1, 1, 1, element_shape, 0);
+	int font_face = cv::FONT_HERSHEY_COMPLEX;
+	double font_scale = 1.5;
+	int thickness = 1;
+	int baseline;
+
+	int elementSize = 5;
+
+	cout << "------program starts------ " << endl;
+
+	if (srcMat.empty())
+	{
+		cout << "load image error!" << endl;
+	}
+
+	//二值化
+	cv::threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+/*	IplImage *image = cvLoadImage(binaryMat);
+	CvMat *imageR = cvCreateMat(512, 512, CV_8UC3);
+	cvResize(image, imageR);
+	cvShowImage("原图", imageR);*/
+	//CvMat *imageD1 = cvCreateMat(512, 512, CV_8UC3);
+	//cvDilate(binaryMat, imageD1, element, 1);//膨胀图像
+	//获得连通域
+	int nComp = cv::connectedComponentsWithStats(binaryMat,
+		labelMat,
+		statsMat,
+		centrMat,
+		8,
+		CV_32S);
+
+	//输出连通域信息
+	for (int i = 0; i < nComp; i++)
+	{
+		//各个连通域的统计信息保存在stasMat中
+		cout << "connected Components NO. " << i << endl;
+		cout << "pixels = " << statsMat.at<int>(i, 4) << endl;
+		cout << "width = " << statsMat.at<int>(i, 2) << endl;
+		cout << "height = " << statsMat.at<int>(i, 3) << endl;
+		cout << endl;
+	}
+
+	//-1,nComp包括背景
+	cout << "the total of connected Components = " << nComp - 1 << endl;
+	//通过颜色表现连通域
+	//显示用图像
+	resultMat = cv::Mat::zeros(srcMat.size(), CV_8UC3);
+	std::vector<cv::Vec3b> colors(nComp);
+	//背景使用黑色
+	colors[0] = cv::Vec3b(0, 0, 0);
+	//使用随机数产生函数randu，随机产生颜色
+	for (int n = 1; n < nComp; n++)
+	{
+		colors[n] = cv::Vec3b(rand() / 255, rand() / 255, rand() / 255);
+	}
+
+	//对所有像素按照连通域编号进行着色
+	for (int y = 0; y < srcMat.rows; y++)
+	{
+		for (int x = 0; x < srcMat.cols; x++)
+		{
+			int label = labelMat.at<int>(y, x);
+			CV_Assert(0 <= label && label <= nComp);
+			resultMat.at<cv::Vec3b>(y, x) = colors[label];
+		}
+	}
+
+	//绘制bounding box
+	for (int i = 1; i < nComp; i++)
+	{
+		char num[10];
+		sprintf_s(num, "%d", i);
+
+		Rect bndbox;
+		//bounding box左上角坐标
+		bndbox.x = statsMat.at<int>(i, 0);
+		bndbox.y = statsMat.at<int>(i, 1);
+		//bouding box的宽和长 
+		bndbox.width = statsMat.at<int>(i, 2);
+		bndbox.height = statsMat.at<int>(i, 3);
+		//绘制
+		rectangle(resultMat, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
+		//连通域编号
+		cv::putText(resultMat, num, Point(bndbox.x, bndbox.y), font_face, 1, cv::Scalar(0, 255, 255), thickness, 8, 0);
+	}
+
+	//将连通域数量绘制在图片上
+	//设置绘制文本的相关参数
+	char text[20];
+	int length = sprintf_s(text, "%d", nComp - 1);
+
+	//获取文本框的长宽
+	cv::Size text_size = cv::getTextSize(text, font_face, font_scale, thickness, &baseline);
+	//将文本框居中绘制
+	cv::Point origin;
+	origin.x = 0;
+	origin.y = text_size.height;
+	cv::putText(resultMat, text, origin, font_face, font_scale, cv::Scalar(0, 255, 255), thickness, 8, 0);
+
+	imshow("binaryMat", binaryMat);
+	imshow("results", resultMat);
+	imshow("frame", srcMat);
+	moveWindow("frame", 0, 20);
+	moveWindow("binaryMat", srcMat.cols, 20);
+	moveWindow("results", srcMat.cols * 2, 20);
+	waitKey(0);
 
 	return 0;
 }
